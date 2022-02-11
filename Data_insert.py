77898e228816f033843919745bc094365d505e35
lee3kv <lee3kv@mail.uc.edu>
2022-02-11 02:44:43 -0500

    Added bme280 information***

--- Data_insert.py
+++ Data_insert.py
@@ -1,9 +1,8 @@
 import sched, time
-sys.path.insert(0, '/home/code/DataDisplay/bme280/adafruit_bme280')
-# from basic as adafruit_bme280
+from adafruit_bme280 import basic as adafruit_bme280
 
 s = sched.scheduler(time.time, time.sleep)
-bme280 = basic.Adafruit_BME280_I2C()
+bme280 = adafruit_bme280.Adafruit_BME280_I2C()
 
 # change this to match the location's pressure (hPa) at sea level
 bme280.sea_level_pressure = 1013.25
