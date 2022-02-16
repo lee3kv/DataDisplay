5f264342b7fcf26413e50875ec77bf0208666b53
lee3kv <lee3kv@mail.uc.edu>
2022-02-16 04:42:02 -0500

    Revised database information

--- server/bme.py
+++ server/bme.py
@@ -5,7 +5,7 @@ from adafruit_bme280 import basic as adafruit_bme280
 # setup for database
 cluster = MongoClient("mongodb+srv://Senior:Senior2022@cluster0.o1ezz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NON")
 db = cluster["Data"]
-collection = db["Test"]
+collection = db["02_16"]
 # setup for scheduler
 s = sched.scheduler(time.time, time.sleep)
 # setup for bme280
@@ -15,6 +15,11 @@ bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 # change this to match the location's pressure (hPa) at sea level
 bme280.sea_level_pressure = 1013.25
 
+# celsius to fahrenheit conversion
+def c_to_f(oldtemp):
+    newtemp = (oldtemp × (9/5)) + 32
+    return(newtemp)
+
 # setup for sending data to database every minute
 def data_upload(sc): 
     # grab latest time every callback
@@ -22,10 +27,13 @@ def data_upload(sc):
     local_time = time.ctime(seconds)
     hour = time.strftime("%I_%M_%S", time.localtime())
 
+    # change celsius to fahrenheit
+    bme280newtemp = c_to_f(bme280.temperature)
+
     # format document for mongodb
     bme280data = {
         hour: {
-            "temp": bme280.temperature, 
+            "temp": bme280newtemp, 
             "humidity": bme280.relative_humidity, 
             "pressure": bme280.pressure,
             "altitude": bme280.altitude
@@ -33,14 +41,14 @@ def data_upload(sc):
     }
 
     # add data to database
-    collection.bme.insert_one(bme280data)
+    collection.insert_one(bme280data)
 
     # console log to show updates
     print("\nUploaded data at... {0}".format(local_time))
-    print("Temperature: {0} C".format(bme280.temperature))
+    print("Temperature: {0} F".format(bme280newtemp))
     print("Humidity: {0} %%".format(bme280.relative_humidity))
     print("Pressure: {0} hPa".format(bme280.pressure))
-    print("Altitude = {0} meters".format(bme280.altitude))
+    print("Altitude: {0} meters".format(bme280.altitude))
     s.enter(60, 2, data_upload, (s,))
 
 # initiates data being sent to database
