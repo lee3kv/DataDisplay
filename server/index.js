d448bb1d0cd45130278065064abb7dff4467af62
lee3kv <lee3kv@mail.uc.edu>
2022-02-24 14:26:04 -0500

    Adding functionality on start up

--- server/index.js
+++ server/index.js
@@ -15,6 +15,6 @@ app.use(cors());
 const CONNECTION_URL = 'mongodb+srv://Senior:Senior2022@cluster0.o1ezz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> app.listen(PORT, ()=> console.log(`connected: ${PORT}`)))
     .catch((error)=> console.log(`not connected:  ${error}`));
